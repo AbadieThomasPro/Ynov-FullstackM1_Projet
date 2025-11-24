d2127b8e40e503153741e3d7592f08f0aee206fa
thomas abadie <thomas.abadiepro@gmail.com>
2025-11-24 10:33:25 +0100

    init des API's

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ynov Fullstack M1 — Application de recettes de cuisine
+
+Le projet FullStack choisit est une application qui permet aux utilsateur d'enregistrer leur propre recette de cuisine.
+
+## Fonctionnalitées:
+
+- Création, modification,
---
+++ api-private/src/server.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json)
+app.get("/", (req, res) => res.send("Hello from API PRIVATE!"));
+app.listen(port, () => console.log(`Server running on port ${port}`));
