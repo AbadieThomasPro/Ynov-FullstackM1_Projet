7c8eb1463c462a7bb0be6354066484a857473316
thomas abadie <thomas.abadiepro@gmail.com>
2025-11-24 13:23:50 +0100

    [API] Ajout log Morgan + eslint

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { setupProxies } from "./proxy.js";
-
+import morgan from "morgan";
 import { ROUTES } from "./routes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // app.use(express.json)
+
+// log en console lisible (dev)
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => res.send("Hello from API Gateway!"));
 
