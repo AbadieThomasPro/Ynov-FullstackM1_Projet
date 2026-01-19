4e51c139175399c3aaccd32911d9a14202681298
Thomas ABADIE <t.abadie@cargo-services.fr>
2026-01-19 15:26:57 +0100

    [APIS] Fix pour fonctionner sur docker

--- api-user/src/config/db.ts
+++ api-user/src/config/db.ts
@@ -3,11 +3,11 @@ import 'dotenv/config';
 
 
 const pool = new Pool({
-    user: process.env.PGUSER_USER,
-    host: process.env.PGHOST_USER,
-    database: process.env.PGDATABASE_USER,
-    password: process.env.PGPASSWORD_USER,
-    port: Number(process.env.PGPORT_USER) || 5442,
+    user: process.env.PGUSER || process.env.PGUSER_USER,
+    host: process.env.PGHOST || process.env.PGHOST_USER || 'db-user',
+    database: process.env.PGDATABASE || process.env.PGDATABASE_USER,
+    password: process.env.PGPASSWORD || process.env.PGPASSWORD_USER,
+    port: Number(process.env.PGPORT || process.env.PGPORT_USER) || 5432,
 });
 console.log("Connexion pool PG créée");
 
